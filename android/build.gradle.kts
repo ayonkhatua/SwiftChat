dfbc3a1b97a6a91204bb3c207d020037d7626505
ayonkhatua <ayonkhatua3011@gmail.com>
2026-02-09 20:07:26 +0530

    Fix: Added google-services plugin version to root gradle

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,15 @@
+// 🟢 1. Plugins Block (Sabse Top par hona chahiye)
+plugins {
+    // Android Application Plugin (Version match karna chahiye aapke setup se)
+    id("com.android.application") version "8.1.0" apply false
+    
+    // Kotlin Plugin
+    id("org.jetbrains.kotlin.android") version "1.8.22" apply false
+
+    // 🟢 YE HAI WO LINE JO MISSING THI (Crash Fix)
+    id("com.google.gms.google-services") version "4.4.1" apply false
+}
+
 allprojects {
     repositories {
         google()
@@ -5,16 +17,16 @@ allprojects {
     }
 }
 
-val newBuildDir: Directory =
-    rootProject.layout.buildDirectory
-        .dir("../../build")
-        .get()
+val newBuildDir: Directory = rootProject.layout.buildDirectory
+    .dir("../../build")
+    .get()
 rootProject.layout.buildDirectory.value(newBuildDir)
 
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
 }
